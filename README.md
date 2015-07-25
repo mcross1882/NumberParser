fcfae169fdc45d0466cfd161f3aff0cd32624687
Matthew Cross <matthew@pmg.co>
2015-07-24 21:44:50 -0500

    Code cleanup: Introduce some lazy test methods to simplify life. Improved the README to match the current code

--- README.md
+++ README.md
@@ -17,29 +17,29 @@ TODO
 
 #### Using the library
 
-`NumberParser` is extremely easy to use and introduces a global method that can be imported called.
-`formatNumber`. It can be executed similar to a `sprintf` statement in `C`.
-
 ```js
+var parser = new NumberParser();
+
 // Format decimal numbers
-assert.equal('123',       formatNumber(123, "%d"));
-assert.equal('0045',      formatNumber(45, "%04d"));
+assert.equal('123',        parser.parseValue(123, "%d"));
+assert.equal('0045',       parser.parseValue(45, "%-04d"));
 
 // Format floating point numbers
-assert.equal('123.46',    formatNumber(123.4567, "%.2f"));
-assert.equal('45.000',    formatNumber(45, "%.3f"));
+assert.equal('123.46',     parser.parseValue(123.4567, "%.2f"));
+assert.equal('45.000',     parser.parseValue(45, "%.3f"));
 
 // Format a currency value (symbols can be freely appended or prepended to the format)
-assert.equal('$123.46',   formatNumber(123.456, "$%.2f"));
+assert.equal('$123.46',    parser.parseValue(123.456, "$%.2f"));
 
 // Format a percentage value
-assert.equal('45.20%',    formatNumber(45.2, "%.2f%"));
+assert.equal('45.20%',     parser.parseValue(45.2, "%.2f%"));
 
 // Add thousands separators using the "s" flag
-assert.equal('10,000',    formatNumber(10000, "%sd"));
-assert.equal('2,345.67',  formatNumber(2345.67, "%sf"));
+assert.equal('10,000',     parser.parseValue(10000, "%sd"));
+assert.equal('2,345.67',   parser.parseValue(2345.67, "%sf"));
 
 // You can also customize the formatting by passing in an options object
-assert.equal('€10.500,25', formatNumber(10500.25, "€%sf", { separator: '.', decimalPoint: ',' }));
+var customParser = new NumberParser({ separator: '.', decimalPoint: ',' });
+assert.equal('€10.500,25', customParser.parseValue(10500.25, "€%sf"));
 ```
 
--- test.js
+++ test.js
@@ -3,57 +3,71 @@ var assert = require('assert');
 
 var globalParser = new NumberParser();
 
-function formatNumber(value, format) {
-    return globalParser.parseValue(value, format);
+function testSuccessCall(expected, value, format) {
+    assert.equal(expected, globalParser.parseValue(value, format));
 }
 
-describe('formatNumber(value, format, options)', function() {
+function testErrorCall(value, format) {
+    assert.throws(function() {
+        globalParser.parseValue(value, format);
+    }, Error);
+}
+
+describe('value, format, options)', function() {
     it('should format decimal values', function() {
-        assert.equal('123', formatNumber(123, "%d"));
-        assert.equal('123000', formatNumber(123, "%06d"));
-        assert.equal('000123', formatNumber(123, "%-06d"));
-        assert.equal('999999123', formatNumber(123, "%-99d"));
+        testSuccessCall('123', 123, "%d");
+        testSuccessCall('123000', 123, "%06d");
+        testSuccessCall('000123', 123, "%-06d");
+        testSuccessCall('999999123', 123, "%-99d");
     });
 
     it('should format floating values', function() {
-        assert.equal('123.45', formatNumber(123.45, "%f"));
-        assert.equal('123.46', formatNumber(123.4567, "%.2f"));
-        assert.equal('100.0',  formatNumber(123.45, "%1.1f"));
-        assert.equal('123.45', formatNumber(123.45, "%12f"));
-        assert.equal('123.45000000', formatNumber(123.45, "%.8f"));
+        testSuccessCall('123.45', 123.45, "%f");
+        testSuccessCall('123.46', 123.4567, "%.2f");
+        testSuccessCall('100.0',  123.45, "%1.1f");
+        testSuccessCall('123.45', 123.45, "%12f");
+        testSuccessCall('123.45000000', 123.45, "%.8f");
     });
 
     it('should format value with symbols', function() {
-        assert.equal('$123.456', formatNumber(123.456, "$%.3f"));
-        assert.equal('45.75%', formatNumber(45.75, "%.2f%"));
+        testSuccessCall('$123.456', 123.456, "$%.3f");
+        testSuccessCall('45.75%', 45.75, "%.2f%");
     });
 
     it('should format integers with separators', function() {
-        assert.equal('100', formatNumber(100, "%sd"));
-        assert.equal('1,000', formatNumber(1000, "%sd"));
-        assert.equal('10,000', formatNumber(10000, "%sd"));
-        assert.equal('100,000', formatNumber(100000, "%sd"));
-        assert.equal('1,000,000', formatNumber(1000000, "%sd"));
-        assert.equal('1,000,000,000', formatNumber(1000000000, "%sd"));
-        assert.equal('1,000,000,000,000', formatNumber(1000000000000, "%sd"));
+        testSuccessCall('100', 100, "%sd");
+        testSuccessCall('1,000', 1000, "%sd");
+        testSuccessCall('10,000', 10000, "%sd");
+        testSuccessCall('100,000', 100000, "%sd");
+        testSuccessCall('1,000,000', 1000000, "%sd");
+        testSuccessCall('1,000,000,000', 1000000000, "%sd");
+        testSuccessCall('1,000,000,000,000', 1000000000000, "%sd");
     });
 
     it('should format floats with separators', function() {
-        assert.equal('100.55', formatNumber(100.55, "%sf"));
-        assert.equal('1,000.55', formatNumber(1000.55, "%sf"));
-        assert.equal('10,000.55', formatNumber(10000.55, "%sf"));
-        assert.equal('100,000.55', formatNumber(100000.55, "%sf"));
-        assert.equal('1,000,000.55', formatNumber(1000000.55, "%sf"));
-        assert.equal('1,000,000,000.55', formatNumber(1000000000.55, "%sf"));
-        assert.equal('1,000,000,000,000.55', formatNumber(1000000000000.55, "%16sf"));
+        testSuccessCall('100.55', 100.55, "%sf");
+        testSuccessCall('1,000.55', 1000.55, "%sf");
+        testSuccessCall('10,000.55', 10000.55, "%sf");
+        testSuccessCall('100,000.55', 100000.55, "%sf");
+        testSuccessCall('1,000,000.55', 1000000.55, "%sf");
+        testSuccessCall('1,000,000,000.55', 1000000000.55, "%sf");
+        testSuccessCall('1,000,000,000,000.55', 1000000000000.55, "%16sf");
     });
 
     it('should not add separators to the remainder', function() {
-        assert.equal('1,000.555555', formatNumber(1000.555555, "%.6sf"));
+        testSuccessCall('1,000.555555', 1000.555555, "%.6sf");
     });
 
     it('should allow of overriding of default symbol formatting', function() {
         var customParser = new NumberParser({ separator: '.', decimalPoint: ',' });
         assert.equal('€10.500,25', customParser.parseValue(10500.25, "€%sf"));
+    });
+
+    it('should throw an error if the format cannot be parsed', function() {
+        testErrorCall(123, "%x");
+        testErrorCall(123, "%abcd");
+        testErrorCall(123, "%&92d");
+        testErrorCall(123, "%2.2d");
+        testErrorCall(123, "%2.2.2f");
     });
 });
